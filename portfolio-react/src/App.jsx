90d5e10646d18ff1a58d61b381e6f8677b04a9d4
usmanbashir1997 <50178849+usman19977@users.noreply.github.com>
2024-04-13 13:34:19 +0500

    Hero Section
    Create Hero section
    Profile Photo
    Description
    Headline
    Tagline
    Connect with me button in hero
    My resume button in hero

--- portfolio-react/src/App.jsx
+++ portfolio-react/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
+import { Hero } from './Components/Hero/Hero'
 
 export default function App() {
     return (
         <div>
             <Navbar />
+            <Hero />
         </div>
     )
 }
