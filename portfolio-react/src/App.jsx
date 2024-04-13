e47c647f5f3454601ed6ccd616bc7dd653827279
usmanbashir1997 <50178849+usman19977@users.noreply.github.com>
2024-04-13 16:21:44 +0500

    Services Section

--- portfolio-react/src/App.jsx
+++ portfolio-react/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import { Hero } from './Components/Hero/Hero'
 import { About } from './Components/About/About'
+import { Services } from './Components/Services/Services'
 
 export default function App() {
     return (
@@ -9,6 +10,7 @@ export default function App() {
             <Navbar />
             <Hero />
             <About />
+            <Services />
         </div>
     )
 }
---
+++ portfolio-react/src/Components/Services/Services.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Services.css';
+import theme_pattern from '../../assets/theme_pattern.svg';
+import arrowIcon from '../../assets/arrow_icon.svg';
+
+import servicesData from '../../assets/services_data';
+export const Services = () => {
+    return (
+        <div className="services">
+            <div className="services-title">
+                <h1>My Services</h1>
+                <img src={theme_pattern} alt="" />
+            </div>
+            <div className="services-container">
+                {servicesData.map((service, index) => {
+                    return <div className="services-format" key={index}>
+                        <h3>{service.s_no}</h3>
+                        <h2>{service.s_name}</h2>
+                        <p>{service.s_desc}</p>
+                        <div className="services-readmore">
+                            <p>Read More</p>
+                            <img src={arrowIcon} alt="" />
+                        </div>
+                    </div>
+                })}
+            </div>
+        </div>
+    )
+}
